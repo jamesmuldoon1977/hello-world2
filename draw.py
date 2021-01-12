4df9595448dc6b55c4915b831046eb9f8bb7e64f
Jmuldoon <jamesmuldoon1977@gmail.com>
2021-01-11 20:54:23 -0500

    add two python script

--- draw.py
+++ draw.py
@@ -1,10 +1,14 @@
 import turtle
 
 painter = turtle.Turtle()
-painter.pensize(20)
 
+
+painter.pensize(20
+	)
 for _ in range(18):
+
 	painter.forward(100)
 	painter.left(100)
 
 turtle.done()
+
--- triangle.py
+++ triangle.py
@@ -5,5 +5,6 @@ painter = turtle.Turtle()
 for _ in range(3):
 	painter.forward(100)
 	painter.left(120)
-
 turtle.done()
+
+
